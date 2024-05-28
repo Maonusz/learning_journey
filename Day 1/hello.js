d7f2f33fbdb955d9eaa8365e4f46985719b35f8f
Maonusz <maonus02@gmail.com>
2024-05-28 23:58:01 +0700

    try using a prime or not a prime for loop-late night practice

--- Day 1/hello.js
+++ Day 1/hello.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+var test = 25; 
+var k =0;
+for (var i = 2; i <= parseInt(test/2); i++) {
+    if (test % i == 0) {
+        console.log("Your inserted number is not a prime number");
+        console.log("please try enter another number");
+        k = 1;
+        break;
+    }
+}
+if (k == 0) {
+    console.log("Your inserted number is a prime number");
+}
