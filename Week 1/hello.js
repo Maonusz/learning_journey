a38e6d32ebb4a0d9e98533d746da2173a4aaefb3
Maonusz <maonus02@gmail.com>
2024-06-07 18:02:30 +0700

    add const variable

--- Week 1/hello.js
+++ Week 1/hello.js
@@ -1,8 +1,8 @@
 console.log("Hello World");
-
+const m =2;
 var test = 3; 
 var k = 0;
-for (var i = 2; i <= parseInt(test/2); i++) {
+for (var i = 2; i <= parseInt(test/m); i++) {
     if (test % i == 0) {
         console.log("Your inserted number is not a prime number");
         console.log("please try enter another number");
@@ -18,3 +18,5 @@ if (k === 0) {
     console.log(hello1);
 } 
 
+
+// new code
