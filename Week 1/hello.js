b402ea9cf4556485d97c4201672492b2c2d7b7e8
Maonusz <maonus02@gmail.com>
2024-06-07 17:52:36 +0700

    learn about let and var

--- Week 1/hello.js
+++ Week 1/hello.js
@@ -1,15 +1,19 @@
 console.log("Hello World");
 
-var test = 25; 
-var k =0;
+var test = 4; 
+var k = 0;
 for (var i = 2; i <= parseInt(test/2); i++) {
     if (test % i == 0) {
         console.log("Your inserted number is not a prime number");
         console.log("please try enter another number");
         k = 1;
+       
         break;
     }
 }
-if (k == 0) {
+
+if (k === 0) {
+    var hello1 = "hello";
     console.log("Your inserted number is a prime number");
-}
+    console.log(hello1);
+} 
